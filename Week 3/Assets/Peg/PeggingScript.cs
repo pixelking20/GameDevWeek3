3cc1a61ebc480f011ff336b2e25fecc258b789c4
LordofHats <Logan.Crosby09@yahoo.com>
2020-07-27 15:58:39 -0400

    Ball and Beg

---
+++ Week 3/Assets/Peg/PeggingScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeggingScript : MonoBehaviour
+{
+    public bool ImportantPeg;
+    public bool MarkedForDeath;
+    public float timer = 10f;
+    public Material hitMat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(MarkedForDeath)
+            timer -= Time.deltaTime;
+
+        if(timer < 0)
+            Destroy(this.gameObject);
+    }
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "ball")
+        {
+            MarkedForDeath = true;
+            gameObject.GetComponent<Renderer>().material = hitMat;
+        }
+    }
+}
