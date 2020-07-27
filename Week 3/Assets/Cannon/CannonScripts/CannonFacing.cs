6e008526e9eeed03b00b2b90cea72cecef2d39b5
Matthew Ramsey <36775051+pixelking20@users.noreply.github.com>
2020-07-27 16:01:09 -0400

    Implemented the Cannon

---
+++ Week 3/Assets/Cannon/CannonScripts/CannonFacing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonFacing : MonoBehaviour
+{
+    
+    RaycastHit RayHit;
+    public float ZLimit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //raycast form the mouse screen position. If the raycast hits an object and the object is further away from the camera than ZLimit, point the Cannon towards it.
+        if (Physics.Raycast(ray, out RayHit))
+            if (RayHit.point.z > ZLimit)
+                transform.forward = new Vector3(RayHit.point.x, RayHit.point.y, 0) - transform.position;
+    }
+}
---
+++ Week 3/Assets/Cannon/CannonScripts/CannonShooting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerShooting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject sphereTemplate;
+    public GameObject spawnedSphere;
+    public float sphereSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")){
+            spawnedSphere  = Instantiate(sphereTemplate, transform.position + transform.forward*2, Quaternion.identity);
+            spawnedSphere.GetComponent<Rigidbody>().AddForce(transform.forward * sphereSpeed);
+            spawnedSphere = null;
+        }
+    }
+}
