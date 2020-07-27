65736855bd41f70eda453db8df85c4013868af49
Matthew Ramsey <36775051+pixelking20@users.noreply.github.com>
2020-07-27 16:07:22 -0400

    Cannon is now shooting and reset scaling

--- Week 3/Assets/Cannon/CannonScripts/CannonShooting.cs
+++ Week 3/Assets/Cannon/CannonScripts/CannonShooting.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TowerShooting : MonoBehaviour
+public class CannonShooting : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject sphereTemplate;
@@ -16,6 +16,7 @@ public class TowerShooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //when the fire control is spressed, we spawn the prefab infront of the Cannon. We then add a force to it's physics body.
         if (Input.GetButtonDown("Fire1")){
             spawnedSphere  = Instantiate(sphereTemplate, transform.position + transform.forward*2, Quaternion.identity);
             spawnedSphere.GetComponent<Rigidbody>().AddForce(transform.forward * sphereSpeed);
