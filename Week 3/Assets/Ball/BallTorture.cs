85cfc93f83c644e18ef6af7e3485257652548af9
LordofHats <Logan.Crosby09@yahoo.com>
2020-07-27 16:48:07 -0400

    BallStuff

---
+++ Week 3/Assets/Ball/BallTorture.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallTorture : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        GameObject[] pegList = GameObject.FindGameObjectsWithTag("peg");
+        foreach(GameObject peg in pegList)
+        {
+            PeggingScript pegScript = peg.GetComponent<PeggingScript>();
+            if(pegScript.markedForDeath)
+            {
+                pegScript.timer = 0.5f;
+            }
+        }
+        Destroy(this.gameObject);
+    }
+}
--- Week 3/Assets/Peg/PeggingScript.cs
+++ Week 3/Assets/Peg/PeggingScript.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class PeggingScript : MonoBehaviour
 {
-    public bool ImportantPeg;
-    public bool MarkedForDeath;
+    public bool importantPeg;
+    public bool markedForDeath;
     public float timer = 10f;
     public Material hitMat;
+    public int importantRate = 25;
+    public Material importantMat;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(Random.Range(0,100) < importantRate)
+        {
+            importantPeg = true;
+            gameObject.GetComponent<Renderer>().material = importantMat;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(MarkedForDeath)
+        if(markedForDeath)
             timer -= Time.deltaTime;
 
         if(timer < 0)
@@ -29,7 +35,7 @@ public class PeggingScript : MonoBehaviour
     {
         if(collision.gameObject.tag == "ball")
         {
-            MarkedForDeath = true;
+            markedForDeath = true;
             gameObject.GetComponent<Renderer>().material = hitMat;
         }
     }
